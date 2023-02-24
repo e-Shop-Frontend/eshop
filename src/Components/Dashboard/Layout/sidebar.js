44ddcbc23221c1ddbc65656d3c2d64a3a1b42d67
Simon Robinson <70141158+Chosen2730@users.noreply.github.com>
2023-02-24 17:08:04 +0100

    added scroll to the sidebar

--- src/Components/Dashboard/Layout/sidebar.js
+++ src/Components/Dashboard/Layout/sidebar.js
@@ -13,32 +13,34 @@ const Sidebar = ({ isSidebarOpen, closeSidebar }) => {
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
       } md:translate-x-0 transition w-[80%] md:w-[30%] lg:w-[20%] bg-sec text-white h-screen p-8 pt-28 z-10`}
     >
-      {menus.map(({ menu, url, img }, i) => {
-        return (
-          <Link
-            className='flex items-center gap-4 my-8'
-            to={url}
-            key={i}
-            onClick={() => {
-              dispatch(setLocation(menu));
-              closeSidebar();
-            }}
-          >
-            <img className='w-5' src={img} alt={menu} />
-            <h2 className='text-sm font-medium'>{menu}</h2>
-          </Link>
-        );
-      })}
-      <Link
-        to='/'
-        className='flex items-center gap-4 mt-20 font-medium'
-        onClick={logout}
-      >
-        <i className='text-xl'>
-          <CiLogout />
-        </i>
-        <h2>Logout</h2>
-      </Link>
+      <div className='h-full overflowHide'>
+        {menus.map(({ menu, url, img }, i) => {
+          return (
+            <Link
+              className='flex items-center gap-4 my-8'
+              to={url}
+              key={i}
+              onClick={() => {
+                dispatch(setLocation(menu));
+                closeSidebar();
+              }}
+            >
+              <img className='w-5' src={img} alt={menu} />
+              <h2 className='text-sm font-medium'>{menu}</h2>
+            </Link>
+          );
+        })}
+        <Link
+          to='/'
+          className='flex items-center gap-4 mt-20 font-medium'
+          onClick={logout}
+        >
+          <i className='text-xl'>
+            <CiLogout />
+          </i>
+          <h2>Logout</h2>
+        </Link>
+      </div>
     </div>
   );
 };
